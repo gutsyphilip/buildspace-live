81e748a45033c1eb05185dc884f3a6f3d121587e
gutsyphilip <philip.c.obosi@gmail.com>
2022-10-07 07:12:11 +0300

    fix: remove wasm config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const path = require("path");
 module.exports = withReactSvg({
   include: path.resolve(__dirname, "assets/svgs"),
   webpack(config, options) {
-    config.experiments = { asyncWebAssembly: true };
     return config;
   },
   reactStrictMode: true,
